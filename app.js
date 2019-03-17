bd902d2c91613a3bbfe3f59ec4b24a23a6c17d2b
ArjoNagelhout <arjo.nagelhout@gmail.com>
2019-03-17 13:35:37 +0100

    Comment rendering done

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-video_path = "/Users/arjonagelhout/Documents/HKU G&I/Blok 3/Project Context/Website/videos"
-
 var app = new Vue({
 			el: '#app',
 			data: {
-				comment_list = [];
+				video_path: "/Users/arjonagelhout/Documents/HKU G&I/Blok 3/Project Context/Website/videos",
+				pictures_path: "/Users/arjonagelhout/Documents/HKU G&I/Blok 3/Project Context/Website/pictures",
+				comments: [
+					{
+						name: "Een persoon", 
+						color: "#0f0", 
+						pictures: ["pictures/1.png", "pictures/3.png", "pictures/6.png"],
+						text: "Dit is een test"
+					}
+				]
 			},
 			methods: {
-				new_bot_comment: function() {
-					this.comment_list.push({})
+				add_comment: function() {
+					this.comments.push({
+							name: "Itz_marcos_Py", 
+							color: "#00f", 
+							picture: "picture_1.png",
+							text: "They just started playing him on the radio up here in Michigan"
+						})
 				}
 			},
 			computed: {
