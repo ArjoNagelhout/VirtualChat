1ca35b2134ecaf7812b5c3a72e177a04485f6679
ArjoNagelhout <arjo.nagelhout@gmail.com>
2019-03-17 17:30:17 +0100

    Finished layout

--- app.js
+++ app.js
@@ -11,23 +11,53 @@ var app = new Vue({
 						pictures: ["1.png", "2.png"]
 					}
 				],
+				positivity: 0,
 				information: {
 
 				},
 				current_video: "movie_2.mp4",
-				comments: []
+				choose: true,
+				current_choice: [
+					{
+						id: 1,
+						text: "Saai, kutstream",
+						result: -2
+					},
+					{
+						id: 2,
+						text: "Superleuk! Ga zo door!",
+						result: 1
+					}
+				],
+				comments: [],
+				counter: 0
 			},
 			methods: {
+				create_player_choice: function() {
+
+				},
+				player_choose: function(choice_id) {
+					var choice = this.current_choice.find(obj => {return obj.id === choice_id});
+
+					this.add_comment(1, choice.text);
+
+					this.positivity += choice.result;
+					this.choose = false;
+				},
 				add_comment: function(commenter_id, text) {
 
 					var commenter = this.commenters.find(obj => {return obj.id === commenter_id});
 
 					this.comments.push({
-							name: commenter.name, 
-							color: commenter.color, 
-							pictures: commenter.pictures,
-							text: text
-						})
+						name: commenter.name, 
+						color: commenter.color, 
+						pictures: commenter.pictures,
+						text: text
+					});
+
+					this.$nextTick(() => {
+  						chat.scrollTop = chat.scrollHeight;
+  					})
 				}
 			},
 			computed: {
